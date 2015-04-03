79e4ed290135c7067f81ca5ef369d1da85a3c259
Keith Pike <whogotbooboo@yahoo.com>
2015-04-02 20:27:41 -0700

    added bower default and some riot json for matches

---
+++ api/controllers/MetricController.js
@@ -0,0 +1,11 @@
+/**
+ * MetricController
+ *
+ * @description :: Server-side logic for managing metrics
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
