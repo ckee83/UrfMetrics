9c4b2ea2120318d6b60f3f64b692ee88dcbe9714
Chris Kee <ckee83@gmail.com>
2015-04-02 20:25:54 -0700

    Scaffolding

---
+++ api/controllers/SummonerController.js
@@ -0,0 +1,11 @@
+/**
+ * SummonerController
+ *
+ * @description :: Server-side logic for managing summoners
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
